54c839f0f2611cbeb21de7e9936c9a450a5b27cc
Divyansh Tripathi <109848779+divy1607@users.noreply.github.com>
2026-03-30 01:04:48 +0530

    vector store basic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod distance;
+mod store;
 fn main() {
 
 }
---
+++ src/store.rs
@@ -0,0 +1,34 @@
+pub struct Store {
+    dim: usize,
+    data: Vec<(u64, Vec<f32>)>,
+    next_id: u64,
+}
+
+impl Store {
+    pub fn new() -> Store {
+        Store {
+            dim: 0,
+            data: Vec::new(),
+            next_id: 0,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn insert(&mut self, vec: Vec<f32>) -> Result<u64, String> {
+        if self.data.is_empty() {
+            self.dim = vec.len();
+        } else {
+            if vec.len() != self.dim {
+                return Err("dimension mismatch".to_string());
+            }
+        }
+        let id = self.next_id;
+        self.next_id += 1;
+        self.data.push((id, vec));
+
+        Ok(id)
+    }
+}
